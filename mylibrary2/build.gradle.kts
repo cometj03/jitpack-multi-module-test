dca545279215e01898e0f891d700154c287d32e4
cometj03 <harryjung1001@gmail.com>
2023-08-21 14:43:30 +0900

    fix

--- mylibrary2/build.gradle.kts
+++ mylibrary2/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
@@ -38,6 +38,9 @@ afterEvaluate {
         publications {
             create<MavenPublication>("maven") {
                 from(components["release"])
+                groupId = "com.cometj.mylibrary1"
+                artifactId = "final"
+                version = "1.0"
             }
         }
     }
